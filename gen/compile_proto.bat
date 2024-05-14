adff402d14eff96834786270ebd8d8f9c9e0e230
HeaInSeo <jwsnsl@knou.ac.kr>
2024-05-14 14:40:23 +0900

    기본 설정 완료

--- gen/compile_proto.bat
+++ gen/compile_proto.bat
@@ -1,13 +1,15 @@
 @echo off
 REM protoc.exe 경로
 set PROTOC_PATH="./protoc.exe"
-REM 프로토콜 버퍼 폴더의 경로  
+REM 프로토콜 버퍼 폴더의 경로
 set PROTO_PATH="../protos"
-REM 프로토콜 버퍼 파일의 경로  
+REM 프로토콜 버퍼 파일의 경로
 set PROTO_FILE="../protos/jobs.proto"
-REM 생성된 파일을 저장할 디렉토리의 경로     
-set OUT_DIR="../protos"                    
-REM 실행 일반 
-REM %PROTOC_PATH% --proto_path=%PROTO_PATH% --go_out=%OUT_DIR% %PROTO_FILE%
-REM 실행 grpc
-%PROTOC_PATH% --proto_path=%PROTO_PATH% --go-grpc_out=%OUT_DIR% %PROTO_FILE%
+REM 생성된 파일을 저장할 디렉토리의 경로
+set OUT_DIR="../protos"
+
+REM 실행 Protocol Buffers 코드 생성
+%PROTOC_PATH% --proto_path=%PROTO_PATH% --go_out=%OUT_DIR% --go_opt=paths=source_relative %PROTO_FILE%
+
+REM 실행 gRPC 코드 생성
+%PROTOC_PATH% --proto_path=%PROTO_PATH% --go-grpc_out=%OUT_DIR% --go-grpc_opt=paths=source_relative %PROTO_FILE%
